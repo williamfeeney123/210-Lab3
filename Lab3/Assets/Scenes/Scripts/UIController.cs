390c4d0a5fb4746ebd302fc1b414055f03421e03
William Feeney <williamfeeney123@gmail.com>
2019-09-26 14:30:42 -0400

    First Commit
    First Commit :+1: :100:  :+1:  :8ball:

---
+++ Lab3/Assets/Scenes/Scripts/UIController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject BallPrefab;
+    public GameObject BallPrefab2;
+    public GameObject BallPrefab3;
+    public GameObject BallPrefab4;
+    public float BallStartYPosition; //Height to spawn balls.
+    public float BallStartXRange;   //Width of random ball spawn position.
+    public Vector3 BoomPosition;    //Point to add force from.
+    public float BoomPower;         //Add force strength.
+    public float BoomFalloff;       //Strength loss over distance.
+
+    private List<GameObject> ballList = new List<GameObject>(); //List of all spawned balls.
+
+    //This is called by the UI button.
+    public void Button_Drop()
+    {
+        //For loop to spawn 10 balls per call.
+        for (int i = 0; i < 1f; i++)
+        {
+            //Instantiate at a random position, and set to temp variable for future use.
+            GameObject newBall = Instantiate(BallPrefab, new Vector3(Random.Range(-BallStartXRange, BallStartXRange), BallStartYPosition, 0f), Quaternion.identity);
+            ballList.Add(newBall); //Use temp varaible to add ball to list.
+        }
+    }
+
+    public void Button_Drop2()
+    {
+        //For loop to spawn 10 balls per call.
+        for (int i = 0; i < 1f; i++)
+        {
+            //Instantiate at a random position, and set to temp variable for future use.
+            GameObject newBall = Instantiate(BallPrefab2, new Vector3(Random.Range(-BallStartXRange, BallStartXRange), BallStartYPosition, 0f), Quaternion.identity);
+            ballList.Add(newBall); //Use temp varaible to add ball to list.
+        }
+    }
+
+    public void Button_Drop3()
+    {
+        //For loop to spawn 10 balls per call.
+        for (int i = 0; i < 1f; i++)
+        {
+            //Instantiate at a random position, and set to temp variable for future use.
+            GameObject newBall = Instantiate(BallPrefab3, new Vector3(Random.Range(-BallStartXRange, BallStartXRange), BallStartYPosition, 0f), Quaternion.identity);
+            ballList.Add(newBall); //Use temp varaible to add ball to list.
+        }
+    }
+
+    public void Button_Drop4()
+    {
+        //For loop to spawn 10 balls per call.
+        for (int i = 0; i < 1f; i++)
+        {
+            //Instantiate at a random position, and set to temp variable for future use.
+            GameObject newBall = Instantiate(BallPrefab4, new Vector3(Random.Range(-BallStartXRange, BallStartXRange), BallStartYPosition, 0f), Quaternion.identity);
+            ballList.Add(newBall); //Use temp varaible to add ball to list.
+        }
+    }
+
+    public void Button_Clear()
+    {
+        //For each loop.
+        foreach (GameObject ball in ballList)
+        {
+            Destroy(ball); //Destroy each ball in the list.
+        }
+        ballList.Clear(); //Clear all balls from the list.
+    }
+
+}
---
+++ Lab3/Assets/Scenes/Scripts/collisionTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Make sure to add this for access to the SceneManagment class.
+public class collisionTester : MonoBehaviour
+{
+    
+
+    //Any Collider2D component will call this function on
+    //any attached scripts when the collider enters a collision with another collider.
+    //The gameobject must also have a RigidBody2D attached.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(2);
+    }
+}
